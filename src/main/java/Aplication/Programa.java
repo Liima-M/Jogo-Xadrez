48f9c4c5f2ccf4d1138e1f1a36617909477d33eb
Liima-M <matheusportinho12@gmail.com>
2021-08-12 19:41:50 -0300

    vamos ver se vai

---
+++ src/main/java/Aplication/Programa.java
@@ -0,0 +1,52 @@
+package Aplication;
+
+import Chess.ChessMatch;
+import Chess.ChessPiece;
+import Chess.ChessPosition;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+//um pequeno teste
+
+public class Programa {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ChessMatch chessMatch = new ChessMatch();
+        List<ChessPiece> captured = new ArrayList<>();
+        
+        while(!chessMatch.getCheckMate()){
+            try {
+                UI.clearScreen();
+                UI.printMatch(chessMatch, captured);
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition source = UI.reaChessPosition(sc);
+                
+                boolean[][] possibleMoves = chessMatch.possibleMoves(source);
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces(), possibleMoves);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition target = UI.reaChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                
+                if(capturedPiece != null){
+                    captured.add(capturedPiece);
+                }
+            }catch(Chess.ChessException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+                
+            }catch(InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+        }
+        
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
+    }
+}
---
+++ src/main/java/Chess/ChessException.java
@@ -0,0 +1,10 @@
+package Chess;
+
+import BoardGame.BoardException;
+
+public class ChessException extends BoardException{
+    
+    public ChessException(String msg){
+        super(msg);
+    }
+}
